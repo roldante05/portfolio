16067aa996a4f289755a1a689319b0ec29e081a6
Dante Roldan <dante@gmail.com>
2022-04-05 19:58:43 -0300

    cambiado

--- index.php
+++ index.php
@@ -24,16 +24,16 @@
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <ul class="navbar-nav me-auto mb-2 mb-md-0 mt-2">
                         <li class="nav-item">
-                            <a class="nav-link active px-4 py-1" aria-current="page" href="index.html">Inicio</a>
+                            <a class="nav-link active px-4 py-1" aria-current="page" href="index.php">Inicio</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="sobre-mi.html">Sobre mí</a>
+                            <a class="nav-link px-4 py-1" href="sobre-mi.php">Sobre mí</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="proyectos.html">Proyectos</a>
+                            <a class="nav-link px-4 py-1" href="proyectos.php">Proyectos</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link px-4 py-1" href="contacto.html">Contacto</a>
+                            <a class="nav-link px-4 py-1" href="contacto.php">Contacto</a>
                         </li>
                     </ul>
 
