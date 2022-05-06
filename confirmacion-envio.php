d75ba5be915c4297794dd2a39b67b751772d5c4d
roldante05 <danteroldan72@gmail.com>
2022-05-06 12:09:56 -0300

    cambios

---
+++ confirmacion-envio.php
@@ -0,0 +1,51 @@
+<?php
+$pg = "contacto";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacto</title>
+    <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
+    <script src="css/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+
+<body id="contacto">
+    <?php include_once "header.php" ?>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 mb-5 mt-3">
+                <h1>Gracias por contactarte!</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-sm-6 ">
+                <p>Me pondre en conctacto contigo a la brevedad</p>
+            </div>
+    </main>
+    <footer class="container col-sm-12 col-12 ">
+        <div class="row">
+            <div class="col-12 col-sm-3 text-center text-sm-start">
+                <a href="https://github.com/roldante05"><i class="fa-brands fa-github "></i></a>
+                <a href="https://www.linkedin.com/in/dante-rold%C3%A1n-a87998201/"><i class="fa-brands fa-linkedin-in"></i></a>
+            </div>
+
+            <div class="col-12 col-sm-3 sponsor text-center texte-sm-start">Sponsor <a href="https://depcsuite.com/">DePC suite</a></div>
+            <div class="col-12 col-sm-3 text-center texte-sm-start"> <a href="mailto:">danteroldan72@gmail.com</a></div>
+        </div>
+        <div class="whatsapp">
+            <a href=""><i class="fa-brands fa-whatsapp"></i></a>
+        </div>
+    </footer>
+
+
+</body>
+
+</html>
